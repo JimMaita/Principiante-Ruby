abd874f7d197327a00fc161195a61dc40c89787c
Jim Darwin Maita <U201423715@upc.edu.pe>
2017-02-21 22:23:29 -0500

    Create dos-números.rb

---
+++ dos-números.rb
@@ -0,0 +1,31 @@
+puts "Give me two numbers and I will tell you all the numbers between them that are prime."
+
+puts "To get started, what is your starting number?
+num = gets.to_i
+
+puts "Excellent, and what is your ending number?"
+last = gets.to_i
+
+puts "Here is the1010 results:"
+
+while (num <= last)
+  prime_flag = true
+  
+  x = 2
+  
+  while (x <= num / 2)
+    if (num % x == 0)
+      prime_flag = false
+    break
+    end
+   
+   x += 1
+  end 
+  
+if prime_flag
+  puts "A prime number there be: " + num.to_s
+end
+
+  num +=1
+  
+end  
